22e233111c0753a5c7be3e2d3bffb1ee64888d30
Mathieu <mathieu.busse@live.fr>
2024-03-17 11:06:47 +0100

    mise en place name et limit en query param

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,15 +1,37 @@
 const { Pokemon } = require('../db/sequelize')
+const { Op } = require('sequelize')
 
 module.exports = (app) => {
     app.get('/api/pokemons', (req, res) => {
-        Pokemon.findAll()
+        const limit = parseInt(req.query.limit) || 100000
+        const name = req.query.name
+
+        if (name) {
+            if (name.length < 2) {
+                return res.status(400).json({ message: "Le terme de recherche doit contenir au minimum 2 caractères." })
+            }
+            return Pokemon.findAndCountAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${name}%`
+                    }
+                },
+                order: ['name'],
+                limit
+            })
+                .then(({ count, rows }) => {
+                    const message = `Il existe ${count} qui correspond au terme de recherche ${name}`
+                    res.json({ message, data: rows })
+                })
+        }
+        Pokemon.findAll({ order: ['name'] })
             .then(pokemons => {
                 const message = 'La liste des pokémons a bien été récupérée.'
                 res.json({ message, data: pokemons })
             })
             .catch(error => {
                 const message = `La liste des pokémons n'a pas pu être récupérée. Réessayer dans quelques instants.`
-                res.status(500).json({message, data: error})
+                res.status(500).json({ message, data: error })
             })
     })
 }
