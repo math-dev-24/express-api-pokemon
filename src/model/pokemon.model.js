a5f4ebea19dd8e94bb56f306ce9072862d90d82d
Mathieu <mathieu.busse@live.fr>
2024-03-17 07:58:26 +0100

    add rest validator

--- src/model/pokemon.model.js
+++ src/model/pokemon.model.js
@@ -7,23 +7,35 @@ module.exports = (sequelize, DataTypes) => {
         },
         name: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notEmpty: {msg: "Le name ne doit pas être vide !"},
+                notNull: {msg: "Le name ne doit pas être null !"}
+            }
         },
         hp: {
             type: DataTypes.INTEGER,
             allowNull: false,
             validate: {
-                isInt: {msg: 'Utilisez uniquement des nombre entiers pour les points de vie.'},
+                isInt: {msg: 'Utilisez uniquement des nombres entiers pour les points de vie.'},
                 notNull : {msg: "Les points de vie sont une propriété requise"}
             }
         },
         cp: {
             type: DataTypes.INTEGER,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isInt: {msg: 'Utilisez uniquement des nombres entiers pour les Cp.'},
+                notNull: {msg: "Les Cp sont une propriété requise"}
+            }
         },
         picture: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                isUrl: {msg: "L'image doit être au format Url"},
+                notNull: {msg: "l'image est requise !"}
+            }
         },
         types: {
             type: DataTypes.STRING,
